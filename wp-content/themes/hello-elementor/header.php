148e270b384327e10346036d57c00661fa94b2ec
Giovanni <giomanara1996@gmail.com>
2022-11-21 15:27:12 +0100

    secondo commit

---
+++ wp-content/themes/hello-elementor/header.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * This is the template that displays all of the <head> section, opens the <body> tag and adds the site's header.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<?php $viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' ); ?>
+	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">	</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<?php hello_elementor_body_open(); ?>
+
+<a class="skip-link screen-reader-text" href="#content">
+	<?php esc_html_e( 'Skip to content', 'hello-elementor' ); ?></a>
+
+<?php
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) {
+	if ( did_action( 'elementor/loaded' ) && hello_header_footer_experiment_active() ) {
+		get_template_part( 'template-parts/dynamic-header' );
+	} else {
+		get_template_part( 'template-parts/header' );
+	}
+}
